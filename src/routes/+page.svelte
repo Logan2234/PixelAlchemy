32e8705c9cd0fa6a5e2c764a258a985952c30471
Logan Willem <logan.willem@grenoble-inp.org>
2025-06-20 22:50:21 +0200

    Improve layout and styling of navigation links in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,13 @@
 
 <h1 class="pt-6 text-center text-3xl font-bold">Welcome to PixelAlchemy</h1>
 
-<div class="flex flex-col gap-4 p-8 sm:flex-row">
+<div class="flex flex-col flex-wrap gap-8 p-8 sm:flex-row">
   {#each links as { href, label, notImplemented = false }}
-    <a href={notImplemented ? '/' : href}>
+    <a href={notImplemented ? '/' : href} class="flex-1 rounded-xl bg-red-50 sm:flex-initial">
       <div
         class="{notImplemented
           ? 'bg-opacity-20 cursor-not-allowed bg-[repeating-linear-gradient(30deg,_#FFFF0044_0_10px,_#00000044_10px_20px)] opacity-75'
-          : 'hover:scale-105 hover:bg-gray-800 hover:text-gray-200'} relative mr-2 flex h-32 w-52 flex-col items-center justify-center rounded-xl border-4 border-gray-800 text-2xl duration-150">
+          : 'hover:scale-105 hover:bg-gray-800 hover:text-gray-200'} relative flex h-32 w-full min-w-52 flex-col items-center justify-center rounded-xl border-4 border-gray-800 text-2xl duration-150 sm:w-52">
         {label}
         {#if notImplemented}
           <span class="absolute bottom-2 text-sm italic">Not implemented yet</span>
