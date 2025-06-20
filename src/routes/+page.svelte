0d939317f642febf2f20ab8813441ec90febdec8
Logan Willem <logan.willem@grenoble-inp.org>
2025-06-20 22:43:26 +0200

    Refactor navigation links layout and improve styling for simulation routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
+  const links = [
+    { href: '/simulation/falling-sand', label: 'Falling Sand' },
+    { href: '/simulation/game-of-life', label: 'Game of Life' },
+    { href: '/simulation/stars', label: 'Moving stars', notImplemented: true }
+  ];
 </script>
 
-<h1 class="text-center text-3xl font-bold">Welcome to PixelAlchemy</h1>
+<h1 class="pt-6 text-center text-3xl font-bold">Welcome to PixelAlchemy</h1>
 
-<ul>
-  <li>
-    <a href="/simulation/falling-sand" class="text-blue-500 hover:underline">Falling Sand</a>
-  </li>
-  <li>
-    <a href="/simulation/game-of-life" class="text-blue-500 hover:underline">Game of Life</a>
-  </li>
-</ul>
+<div class="flex flex-col gap-4 p-8 sm:flex-row">
+  {#each links as { href, label, notImplemented = false }}
+    <a href={notImplemented ? '/' : href}>
+      <div
+        class="{notImplemented
+          ? 'bg-opacity-20 cursor-not-allowed bg-[repeating-linear-gradient(30deg,_#FFFF0044_0_10px,_#00000044_10px_20px)] opacity-75'
+          : 'hover:scale-105 hover:bg-gray-800 hover:text-gray-200'} relative mr-2 flex h-32 w-52 flex-col items-center justify-center rounded-xl border-4 border-gray-800 text-2xl duration-150">
+        {label}
+        {#if notImplemented}
+          <span class="absolute bottom-2 text-sm italic">Not implemented yet</span>
+        {/if}
+      </div>
+    </a>
+  {/each}
+</div>
--- src/routes/simulation/+layout.svelte
+++ src/routes/simulation/+layout.svelte
@@ -8,7 +8,7 @@
   href="/"
   class="group absolute top-6 left-6 flex cursor-pointer items-center justify-end gap-1 text-sm text-gray-200">
   <ChevronLeft class="w-5 transition-all duration-150 group-hover:-translate-x-1" />
-  <span>Retour</span>
+  <span class="pb-0.5">Retour</span>
 </a>
 
 <div class="h-full w-full text-gray-200">
